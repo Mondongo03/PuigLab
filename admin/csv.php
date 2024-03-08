1f5f3c7b7a301238f050ff374f45baf237a36054
Rubén <rjulian@elpuig.xeill.net>
2024-03-08 08:44:39 +0000

    limpio

---
+++ admin/csv.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<head>
+<!-- Font awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Bootstrap Datatables -->
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<!-- Bootstrap social button library -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<!-- Bootstrap select -->
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<!-- Bootstrap file input -->
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<!-- Awesome Bootstrap checkbox -->
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<!-- Admin Stye -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="ts-main-content"> 
+<?php include('includes/leftbar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+
+						<h2 class="page-title">Envía tu csv</h2>
+						
+						<div class="row">
+							<div class="col-md-12">
+								<div class="row">
+									<div class="col-md-3">
+										<div class="panel panel-default">
+											<div class="panel-body bk-primary text-light">
+												<div class="stat-panel text-center">
+
+<form action="Upload.php" method="post" enctype="multipart/form-data">
+  Selecciona un archivo CSV:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Subir archivo" name="submit">
+</form>
+</body>
+</html>
